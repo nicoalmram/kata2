4127c9788a0aba64d3447f672752e0d77fb0e8b0
Usuario <Usuario@10.22.148.84>
2019-10-02 09:39:33 +0100

    second commit

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,12 @@
+package kata2;
+
+public class Histogram {
+    private final Map<Integer, Integer> counts = new HashMap<>();
+    
+    public Histogram(int... values) {
+        for (int value : values) {
+            
+        }
+    }
+    
+}
